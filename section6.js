15da91e4c9b12ab8b5b62801f209cfba11384c50
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-06 21:14:39 -0600

    open console, declare variable, if statement, calculator.

---
+++ section6.js
@@ -0,0 +1,16 @@
+// you may use the console in the web browser by pressing command + option + j.  
+// typed in console
+alert("hello world") //notification popped up from chrome tab w/message
+undefined
+let js = 'amazing' //declare a variable
+undefined
+if (js === 'amazing') alert('This is cool') // if (condition) action
+undefined
+///
+
+//redeclare variable
+js = 'boring'
+'boring'
+if (js === 'amazing') alert('This is cool') //no alert
+
+undefined
